405ba49e216a07ff629003a87d7b1abb42cc97ef
shalini rajput <rajputshalini8744@gmail.com>
2025-08-01 02:59:44 +0530

    configure upload with file type and size restrictions

--- cloudConfig.js
+++ cloudConfig.js
@@ -1,5 +1,7 @@
 const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const multer = require("multer");
+const path = require("path");
 
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
@@ -9,13 +11,41 @@ cloudinary.config({
 
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
-    params: {
-        folder: "Medical_Report",
-        allowerdformets: ["pdf", "jpg"]
+    params: async (req, file) => {
+        const allowedFormats = ["image/jpg", "image/png", "image/jpeg", "application/pdf", "application/vnd.openxmlformats-officedocument.wordprocessingml.document"];
+        console.log("This is mime type" ,file.mimetype);
+        if(!allowedFormats.includes(file.mimetype)) {
+            const err = new Error("Given file format not allowed");
+            err.http_code = 400;
+            throw err;
+        }
+
+         // resource_type based on file type
+        const isPdf = file.mimetype === "application/pdf";
+        const isDocx = file.mimetype === "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
+
+        return {
+            folder: "Medical_Report",
+            public_id: `${Date.now()}-${(req.user && req.user._id) || "unknown"}`,
+            resource_type: isPdf || isDocx ? "raw" : "image",
+        }
+    }
+});
+
+const upload = multer({
+    storage,
+    limits: { fileSize: 5 * 1024 * 1024 },
+    fileFilter: (req, file, cb) => {
+        console.log("this is a file" ,file.originalname);
+        const allowed = /pdf|jpg|jpeg|png|docx/;
+        const ext = path.extname(file.originalname).toLowerCase();
+            if (allowed.test(ext)) cb(null, true);
+            else cb(new Error("Invalid file type"));
     }
 });
 
 module.exports = {
+    upload,
     cloudinary,                            //from cloudinary.Config 
     storage,
 }
