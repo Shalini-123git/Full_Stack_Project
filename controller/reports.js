2426c2eb37314226c2def4335befcfac09ff5ac2
shalini rajput <rajputshalini8744@gmail.com>
2025-08-08 17:27:36 +0530

    Extract reports route logic to controller

---
+++ controller/reports.js
@@ -0,0 +1,89 @@
+const Report = require("../models/reports.js");
+
+//Create New Report
+module.exports.newReport = (req, res) => {
+    res.render("reports/create.ejs");
+}
+
+//Post New Report
+module.exports.create = async(req, res) => {
+    try {
+        if (!req.user) {
+        return res.status(401).json({ message: "Unauthorized: Invalid token" });
+        }
+        const userId = req.user.user._id;
+        const { title, description} = req.body;
+        if (!req.file) return res.status(400).send("No file uploaded");
+        const { path: url, filename } = req.file;
+
+        const report = new Report({
+            userId,
+            title,
+            description,
+            file: {
+                url,
+                filename,
+            }
+        });
+        
+        await report.save();      //save report
+
+        res.redirect("/report");
+
+    } catch (err) {
+        console.error("Error uploading report:", err);
+        res.status(500).json({message: err.message});
+    }
+}
+
+//index route
+module.exports.index = async (req, res) => {
+    const userId = req.user.user._id;
+    console.log("this is current user", userId);
+    const reports = await Report.find({userId}).populate("userId");
+    console.log(reports);
+    res.render("reports/index.ejs", {reports, userId});
+}
+
+//Show Report
+module.exports.show =  async (req, res) => {
+    const { id } = req.params;
+    const report = await Report.findById(id);
+    res.render("reports/show.ejs", {report});
+}
+
+//Edit Report
+module.exports.edit = async (req, res) => {
+    try {
+    const report = await Report.findById(req.params.id);
+
+    if (!report) {
+        res.redirect("/report");
+    }
+
+    res.render("reports/edit.ejs", { report});
+  } catch (err) {
+    res.status(500).send("Server Error");
+  }
+}
+
+//Update Report
+module.exports.update = async (req, res) => {
+    let { id } = req.params;
+    let report = await Report.findByIdAndUpdate(id, {...req.body});
+    if(typeof req.file !== "undefined"){
+        let url = req.file.path;
+        let filename = req.file.filename;
+        report.file = { url, filename };
+        await report.save();
+    }
+    res.redirect(`/report`);
+}
+
+//Delete Report
+module.exports.delete = async (req, res) => {
+    const { id } = req.params;
+    await Report.findByIdAndDelete(id);
+    res.redirect("/report");
+}
+
