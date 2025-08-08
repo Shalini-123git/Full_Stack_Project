6f8b90251d43208bcb590aab0602790f5b4e1293
shalini rajput <rajputshalini8744@gmail.com>
2025-08-08 17:21:16 +0530

    Refactor reports route to controller

--- routes/reports.js
+++ routes/reports.js
@@ -1,72 +1,37 @@
 const express = require("express");
 const router = express.Router();
-const auth = require("../middleware/auth.js");
-const Report = require("../models/reports.js");
+const {cookieJwtAuth} = require("../middleware/auth.js");
 const {upload} = require("../cloudConfig.js");
-const { authenticateUser, createReportValidator } = require("../middleware/validation.js");
+const { createReportValidator } = require("../middleware/validation.js");
+const cookieParser = require("cookie-parser");
+const reportController = require("../controller/reports.js");
 
-router.use(auth);
-
-//index route
-router.get("/",(req, res) => {
-    res.render("reports/index.ejs");
-
-})
-
-//show report
-router.get("/show", async (req, res) => {
-    res.render("reports/show.ejs");
-});
+router.use(cookieParser());
+router.use(cookieJwtAuth);
 
 //add new report
-router.get("/create", (req, res) => {
-    res.render("reports/create.ejs");
-});
+router.route("/create")
+    .get(reportController.newReport)
+    .post(
+        upload.single("file"), 
+        createReportValidator, 
+        reportController.create
+    )
 
-//post report
-router.post("/create", authenticateUser, upload.single("file"), createReportValidator, async(req, res) => {
-    try {
-        if (!req.user || !req.user.userId) {
-        return res.status(401).json({ message: "Unauthorized: Invalid token" });
-        }
-        const { userId } = req.user;
-        const { title, description} = req.body;
-        console.log(title, description);
-        if (!req.file) return res.status(400).send("No file uploaded");
-        const { path: url, filename } = req.file;
+//index route
+router.get("/", reportController.index)
 
-        const report = new Report({
-            userId,
-            title,
-            description,
-            file: {
-                url,
-                filename,
-            }
-        });
-        console.log(report);
-        await report.save();      //save report
-
-        res.render("reports/show.ejs", {report});
-
-    } catch (err) {
-        console.error("Error uploading report:", err);
-        res.status(500).json({message: err.message});
-    }
-})
+//show report - update report - delete report
+router.route("/:id")
+    .get(reportController.show)
+    .put(
+        upload.single("file"), 
+        createReportValidator, 
+        reportController.update
+    )
+    .delete(reportController.delete)
 
 //edit report
-router.get("/edit", (req, res) => {
-    res.render("reports/edit.ejs");
-});
-
-router.put("/edit", (req, res) => {
-    res.send("edited");
-})
-
-//delete report
-router.delete("/delete", (req, res) => {
-    res.send("Report deleteed");
-});
+router.get("/:id/edit", reportController.edit)
 
 module.exports = router;
