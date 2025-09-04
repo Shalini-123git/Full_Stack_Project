cfa4b1b09ef40c34e95de9d8209c5c385d3d843c
shalini rajput <rajputshalini8744@gmail.com>
2025-09-04 20:03:01 +0530

    update

--- routes/admin.js
+++ routes/admin.js
@@ -11,27 +11,27 @@ const Checklist = require("../models/checklist.js");
 router.use(cookieParser());
 router.use(cookieJwtAuth);
 
-router.get("/report", restrictTo("doctor"), async (req, res) => {
+router.get("/report", restrictTo("doctor", "admin"), async (req, res) => {
     const reports = await Report.find({});
     res.render("reports/index.ejs", {reports});
 })
 
-router.get("/medicalHistory", restrictTo("doctor"), async (req, res) => {
+router.get("/medicalHistory", restrictTo("doctor", "admin"), async (req, res) => {
     const medicalHistories = await MedicalHistory.find({});
     res.render("medicalHistory/index.ejs", {medicalHistories});
 })
 
-router.get("/timeline", restrictTo("doctor"), async (req, res) => {
+router.get("/timeline", restrictTo("doctor", "admin"), async (req, res) => {
     const timeline = await Timeline.find({});
     res.render("timeline/index.ejs", {timeline});
 })
 
-router.get("/checklist", restrictTo("doctor"), async (req, res) => {
+router.get("/checklist", restrictTo("doctor", "admin"), async (req, res) => {
     const checklist = await Checklist.find({});
     res.status(200).json({message: checklist});
 })
 
-router.get("/appointments", restrictTo("doctor"), async (req, res) => {
+router.get("/appointments", restrictTo("doctor", "admin"), async (req, res) => {
     const appointment = await Appointment.find({}).populate("mother");
     res.render("appointment/index.ejs", {appointment});
 })
